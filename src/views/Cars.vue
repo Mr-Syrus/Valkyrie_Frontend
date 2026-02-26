0b36a0233a4c1f879761d50cf0dfc5f0bdebd9c1
catmoska <84236986+catmoska@users.noreply.github.com>
2026-02-26 11:02:38 +0200

    q

--- src/views/Cars.vue
+++ src/views/Cars.vue
@@ -638,7 +638,7 @@ onUnmounted(() => {
             </div>
 
             <div class="mb-3">
-              <label class="form-label">Площадки<span class="text-danger">*</span></label>
+              <label class="form-label">Площадка<span class="text-danger">*</span></label>
               <input
                   v-model="form.platformSearch"
                   type="text"
